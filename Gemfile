e06e03b13c2aca812929170c175104f7b27c015c
onizenso <onicrypt@gmail.com>
2015-01-02 17:50:52 -0800

    Added OAuth gem and basic method for AccessToken
    Used template method from Twitter developer site
    for retrieving an instance of the AccessToken needed to
    authenticate to the API and make requests.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 group :development, :test do
   gem 'httparty'
+  gem 'oauth', :git => 'https://github.com/oauth-xx/oauth-ruby.git'
   gem 'rack'
   gem 'guard'
   gem 'guard-rspec'
--- api.rb
+++ api.rb
@@ -1,1 +1,19 @@
 require 'httparty'
+require 'oauth'
+
+class TwiTransfer
+  include HTTParty
+
+  def get_access_token(oauth_token, oauth_token_secret)
+    consumer = OAuth::Consumer.new("APIKey", "APISecret", 
+                                  { :site => "https://api.twitter.com", 
+                                    :scheme => :header })
+     
+    # now create the access token object from passed values
+    token_hash = { :oauth_token => oauth_token, 
+                   :oauth_token_secret => oauth_token_secret }
+    access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+ 
+    return access_token
+  end 
+end
